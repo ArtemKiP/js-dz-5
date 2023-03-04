6230e9b6f80ed4338216c0e74d22affac74d1bee
ArtemKiP <der.pap@mail.ru>
2023-03-04 16:38:35 +0600

    5-hm

---
+++ main.js
@@ -0,0 +1,99 @@
+// const task1 = (arr) => {
+// return arr.map ((item) => {
+//     return item * 2
+// })
+// };
+//
+// console.log(task1([1, 2, 3]));
+
+//
+// const task2 = (arr) => {
+// return arr.filter((item,idx) => {
+// if (idx % 2 === 0){
+//     return item
+// }
+//
+//
+// })
+// };
+//
+// console.log(task2([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+
+// const task3 = (arr) => {
+// return arr.map((item,) => {
+// return item * -1
+// })
+// };
+//
+// console.log(task3([0,1, 2, 3, 4, 5]));
+
+// const task4 = (arr,num) => {
+// return arr.filter((item) => {
+//     return item < num
+// })
+// };
+// console.log(task4([0, 1, 2, 3, 5, 8, 13], 5));
+
+// const task5 = (arr) => {
+// return arr.filter((item,idx) => {
+// return item % idx === 0
+// })
+// };
+// console.log(task5([11, -11]));
+
+
+// const task6 = (arr) => {
+// return arr.map((item) =>{
+// if (typeof item === 'string'){
+//     return 'null'
+// }
+// return item
+// })
+// };
+//
+// console.log(task6([4, 6, 'Ivan', 5, 'Denis ']));
+
+
+// const task7 = (arr) => {
+// return arr.filter((item) =>{
+//     return item.length <= 5
+// })
+// };
+//
+// console.log(task7(['Евдоким','Ivan', 'Игнат', 'Denis']));
+
+// const task8 = (arr) =>{
+// return arr.map ((item) => {
+//     return item ** 3
+// }).map((el) =>{
+//     if (el % 3 === 0 && el % 5 === 0){
+//         return 0
+//     }
+//     return el
+// })
+// };
+//
+// console.log(task8([7, 8, 15, 30, 2] ));
+
+
+// const task9 = (arr) => {
+// return arr.map((item) => {
+//     return item.length
+// })
+// };
+// console.log(task9(['Термос', 'Ураган', 'Земля', 'Комбо', 'Корень']));
+
+
+// const task10 = (arr) => {
+// return arr.map((item) => {
+//     return `<div>${item}</div>`
+// })
+// };
+// console.log(task10([1, 2, 3, 4, 5 ] ));
+
+// const task11 = (arr) => {
+// return arr.filter((item) => {
+//     return item.length % 2 ===0
+// })
+// };
+// console.log(task11(['ab', 'abc', 'abcd']));
